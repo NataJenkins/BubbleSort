ed5e5c4dbb41d008f19cf0a2575835937c4fc9a2
Natalia <natajenkins@hotmail.com>
2020-10-12 10:09:04 -0500

    Linter fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,32 @@
-def bubble_sort_by (array)
-    i = 0
-    length = array.length-2
-    while i <= length
-        if array[i] > array[i+1]
-            array[i], array[i+1] = array[i+1], array[i]  
-            i=-1
-        end
-        i+=1
+def bubble_sort_by(array)
+  i = 0
+  length = array.length - 2
+  while i <= length
+    if array[i] > array[i + 1]
+      array[i], array[i + 1] = array[i + 1], array[i]
+      i = -1
     end
-    print array
+    i += 1
+  end
+  print array
 end
 
-#first example for number
+# first example for number
 bubble_sort_by([9, 7, 8, 4, 2, 1, 3, 5, 6])
 
-
-#Yield
+# Yield
 def bubble_sort(array)
-    i = 0
-    length = array.length-2
-    while i <= length
-        if yield(array[i],array[i+1]).positive?
-            array[i], array[i+1] = array[i+1], array[i]  
-            i=-1
-        end
-        i+=1
+  i = 0
+  length = array.length - 2
+  while i <= length
+    if yield(array[i], array[i + 1]).positive?
+      array[i], array[i + 1] = array[i + 1], array[i]
+      i = -1
     end
-    print array
+    i += 1
+  end
+  print array
 end
 
-#second example string's length
+# second example string's length
 bubble_sort(%w[orange car apple]) { |str1, str2| str1.length - str2.length }
-
-
-
-
-
