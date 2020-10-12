10907dec1296bb9c465d8ade11730ef54c0d492a
Ahmet Bozaci <ahmt9417@gmail.com>
2020-10-12 17:59:21 +0300

    added new function for str length with yield

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,8 @@
 def bubble_sort_by (array)
     i = 0
-    length = array.length
+    length = array.length-2
     while i <= length
-        if array[i+1] != nil && array[i] > array[i+1]
+        if array[i] > array[i+1]
             array[i], array[i+1] = array[i+1], array[i]  
             i=-1
         end
@@ -12,8 +12,25 @@ def bubble_sort_by (array)
 end
 
 #first example for number
-bubble_sort_by ([9, 7, 8, 4, 2, 1, 3, 5, 6])
+bubble_sort_by([9, 7, 8, 4, 2, 1, 3, 5, 6])
 
+
+#Yield
+def bubble_sort(array)
+    i = 0
+    length = array.length-2
+    while i <= length
+        if yield(array[i],array[i+1]).positive?
+            array[i], array[i+1] = array[i+1], array[i]  
+            i=-1
+        end
+        i+=1
+    end
+    print array
+end
+
+#second example string's length
+bubble_sort(%w[orange car apple]) { |str1, str2| str1.length - str2.length }
 
 
 
